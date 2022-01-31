bd64ee01721d4ba038104b4a62b6a9984d785380
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-01-31 22:12:33 +0530

    comments added user signup conditions modded

--- controller/routes.js
+++ controller/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
+const users = require('../model/user.js');
 const router = express.Router();
+var bcrypt = require('bcryptjs');
 
 router.get("/", (req, res) => {
     res.render("index");
@@ -15,21 +16,48 @@ router.get("/login", (req, res) => {
 
 router.post("/signup", (req,res) =>{
     const {email, username, password, confirmpassword } = req.body;
-
+//checks if all fields are filled
     if (!email || !username || !password || !confirmpassword){
         res.render('signup', {
             csrfToken: req.csrfToken(), 
             err: "All fields are required!"
         });
     }
+//checks if passwords are matching
     else if ( password != confirmpassword ){
         res.render('signup', {
             csrfToken: req.csrfToken(), 
             err: "Password should be same!",
     });
 }
+//checks if diff person is having that email or username
+    else{
+        var userData = await users.findOne({ 
+            $or : [ { email : email}, {username: username}] })
+    };
+
+//if userData is valid or true then tell that user exists
+    
+    if (userData){
+        res.render('signup', {
+            csrfToken: req.csrfToken(), 
+            err: "User exists, try Logging in",
+    });
+    }
+
+//else hash the password using bcrypt and save in db, redirect to login    
     else{
 
+        var salt = bcrypt.genSalt(12);
+        var hash = bcrypt.hash(password, salt);
+
+        users({
+            email:email, 
+            username:username,
+            password:hash
+        }).save();
+
+        res.redirect('/login');
     }
 });
 
